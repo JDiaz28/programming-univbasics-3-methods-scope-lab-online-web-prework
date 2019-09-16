b16cf4310767ef75a37904ea2af85aba46a75388
Abraham Moreno <adiaz2821@gmail.com>
2019-09-16 13:12:18 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,8 +1,14 @@
 def mario
-  status = 'Thank You Mario! But Our Princess Is In Another Castle!'
+  phrase = 'Its-a me, Mario!'
   puts phrase
 end
 
 def toadstool
+  status = 'Thank You Mario! But Our Princess Is In Another Castle!'
   puts status
 end
+
+def any_phrase(phrase)
+  phrase = 'Do a barrel roll!'
+end
+  
